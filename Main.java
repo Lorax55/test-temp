5f96767af3d8c74fe33021a41fa8d6fddab6247b
Lorax55 <vladyslav.usa@gmail.com>
2024-12-04 19:36:56 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World!");
+
+        //for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+          //  System.out.println("i = " + i);
+            System.out.println("++++++++++");
+
+        System.out.println(((100 + 15) / 3) + "Test");
+
+        int num1 = 100;
+        int num2 = 15;
+        int result = num1 + num2 / 3;
+        System.out.println(result);
+        }
+    }
